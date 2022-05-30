6c3efab756fc0848fc7278831b7b70398de635ee
Obiagba Mary Ifeoma <marychord@gmail.com>
2022-05-30 13:41:39 +0100

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,20 @@
+export function canExecuteFastAttack(knightIsAwake) {
+
+}
+
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+
+}
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+
+}
+
+export function canFreePrisoner( 
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+ ) {
+
+}
